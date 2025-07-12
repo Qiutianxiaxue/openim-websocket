7c1b99a20bf2aa864d60fd8530d59dd870b37841
Qiutianxiaxue <qiutianxiaxue@hotmail.com>
2025-07-12 14:35:09 +0800

    支持req导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'OpenIMWebSocket',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "OpenIMWebSocket",
       fileName: (format) => `openim-websocket.${format}.js`,
-      formats: ['es', 'umd']
+      formats: ["es", "umd", "cjs"],
     },
     rollupOptions: {
-      external: ['ws'],
+      external: ["ws"],
       output: {
         globals: {
-          ws: 'ws'
+          ws: "ws",
         },
-        format: 'es'
-      }
-    }
-  }
-}); 
+      },
+    },
+  },
+});
